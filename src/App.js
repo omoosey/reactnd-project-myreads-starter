4725f231e4148454d71b011731fc301ce4f8889e
Mariano Salamanca Jr <msalamancajr@gmail.com>
2018-09-23 22:07:40 -0400

    Testing of BookAPI

--- src/App.js
+++ src/App.js
@@ -7,18 +7,26 @@ import ListBooks from './ListBooks'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   searchBooks = (book) => {
     BooksAPI.search(book).then((book) => {
-      console.log(book)
+      book.map((book) => {
+        console.log(book.id)
+      })
+    })
+  }
+
+  allBooks = () => {
+    BooksAPI.getAll().then((books) => {
+      console.log(books)
     })
   }
 
@@ -26,12 +34,13 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-      {this.searchBooks('Space')}
         <Route path="/search" exact render={() => (
           <SearchBooks/>
         )}/>
         <Route path="/" exact render={() => (
-          <ListBooks/>
+          <ListBooks
+            books={this.state.books}
+          />
         )}/>
       </div>
     )
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,8 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 class ListBooks extends React.Component {
+	addSelect = () => {
+		return (
+		<select>
+	        <option value="move" disabled>Move to...</option>
+	        <option value="currentlyReading">Currently Reading</option>
+	        <option value="wantToRead">Want to Read</option>
+	        <option value="read">Read</option>
+	        <option value="none">None</option>
+	      </select>
+      	)
+	}
+
 	render() {
-		return (<div className="list-books">
+		let currentBooks = this.props.books
+		console.log(currentBooks)
+
+		return (
+			<div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
@@ -18,13 +34,7 @@ class ListBooks extends React.Component {
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                             <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
+                              {this.addSelect()}
                             </div>
                           </div>
                           <div className="book-title">To Kill a Mockingbird</div>
