368025f95b76443d57ca5901ad51213c36569508
Mariano Salamanca Jr <msalamancajr@gmail.com>
2018-09-23 20:22:26 -0400

    Added basic react routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
+import { Route, Link } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
@@ -13,31 +14,21 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+  searchBooks = (book) => {
+    BooksAPI.search(book).then((book) => {
+      console.log(book)
+    })
+  }
+
+
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
+      {this.searchBooks('To')}
+        <Route path="/search" exact render={() => (
+          
+        )}/>
+        <Route path="/" exact render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -194,10 +185,10 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to="/search">Add a book</Link>
             </div>
           </div>
-        )}
+        )}/>
       </div>
     )
   }
