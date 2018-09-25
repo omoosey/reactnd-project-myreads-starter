1d1abb2189ffce18952662839c39d5cdefdbebe0
Mariano Salamanca <msalamancajr@gmail.com>
2018-09-25 13:44:19 -0400

    Working on shelf selection

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,17 +1,9 @@
 import React from 'react';
 
 class ListBooks extends React.Component {
-	addSelect = () => {
-		return (
-		<select>
-	        <option value="move" disabled>Move to...</option>
-	        <option value="currentlyReading">Currently Reading</option>
-	        <option value="wantToRead">Want to Read</option>
-	        <option value="read">Read</option>
-	        <option value="none">None</option>
-	      </select>
-      	)
-	}
+	handleChange = (event) => {
+    console.log(event.target.value);
+  }
 
 	render() {
 		let currentBooks = this.props.books
@@ -27,7 +19,13 @@ class ListBooks extends React.Component {
             <div className="book-top">
               <div className="book-cover" style={{width: 128, height: 193, backgroundImage: 'url("'+book.imageLinks.thumbnail+'")'}}></div>
               <div className="book-shelf-changer">
-                {this.addSelect()}
+                <select onChange={this.handleChange}>
+                  <option value="move" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
               </div>
             </div>
             <div className="book-title">{book.title}</div>
